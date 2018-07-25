26347643d9848f7d169e5a0ba6cdec5dcf2077e9
Nguyen Van Son <hailos1000@gmail.com>
2018-07-25 23:38:40 +0700

    update

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ app.use(express.static(path.join(__dirname, 'app_client')));
 app.use('/api', routesApi);
 
 app.use(function(req, res) {
-  res.sendFile(path.join(__dirname, 'app_client', 'index.html'), { root: __dirname });
+  res.sendFile(path.join(__dirname, 'app_client', 'index.html'));
 });
 
 // catch 404 and forward to error handler
